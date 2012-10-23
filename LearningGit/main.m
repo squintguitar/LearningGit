2e37d60dd93db522686d831112b612a594430316
Matt Fredrickson <matt@binaryconcepts.com>
2012-10-23 15:29:26 -0500

    Initial Commit

---
+++ LearningGit/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LearningGit
+//
+//  Created by Matt Fredrickson on 10/23/12.
+//  Copyright (c) 2012 Matt Fredrickson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
